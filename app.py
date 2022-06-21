754158057abce40151e0e147c890da669b623bf6
ryanrsanchez <ryanrsanchezt@gmail.com>
2022-06-21 17:03:47 -0400

    update

--- app.py
+++ app.py
@@ -54,8 +54,7 @@ def user_login():
     else:
         db_user_login.commit()
         db_user_login.close()
-        import sys
-        print ('Incorrect username/password!',file=sys.stderr)
+        
         return render_template('login.html')
 
 #AQUI PREPARÉ EL REGISTRO
